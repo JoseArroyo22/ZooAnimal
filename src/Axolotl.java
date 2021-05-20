aa6db0d5c5554e11ff9f519bddbdf827b8c6290f
JoseArroyo22 <77021316+JoseArroyo22@users.noreply.github.com>
2021-05-20 14:31:41 -0700

    .

--- src/Axolotl.java
+++ src/Axolotl.java
@@ -1,2 +1,27 @@
-public class Axolotl {
+public class Axolotl implements  iAnimal {
+
+
+    public String getAnimalType() {
+        return "Axolotl";
+    }
+
+
+    public int getIdTag() {
+        return 0;
+    }
+
+
+    public void setIdTag(int anIdTag) {
+
+    }
+
+
+    public int getMinTemperature() {
+        return 50;
+    }
+
+
+    public int getMaxTemperature() {
+        return 74;
+    }
 }
